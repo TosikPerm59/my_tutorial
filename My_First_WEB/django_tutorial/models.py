730f7a60732e7cea4dfd3e81bebcb350a446ad75
TosikPerm59 <tosikperm59@gmail.com>
2021-06-22 21:14:35 +0500

    В django_tutorial добавлены файлы index.html, base.html, tree_list.html

--- My_First_WEB/django_tutorial/models.py
+++ My_First_WEB/django_tutorial/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    name = models.CharField
+    content = models.TextField
+    child_id = None
+
+
+
+
+
