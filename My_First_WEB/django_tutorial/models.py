67181e3a1a9410824bf4e9f889b1be07aa4a3340
TosikPerm59 <tosikperm59@gmail.com>
2021-07-08 21:13:21 +0500

    Добил левую часть с древовидным выпадающим списком, добавление статей через админ панель

--- My_First_WEB/django_tutorial/models.py
+++ My_First_WEB/django_tutorial/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class Article(models.Model):
-    name = models.CharField
-    content = models.TextField
-    child_id = None
+    name = models.CharField(max_length=30)
+    content = models.TextField(blank=True)
+    parent_name = models.CharField(max_length=30, default=0)
+    date_of_change = models.DateField(auto_now=True)
+    status = models.CharField(max_length=30, default=0)
+    childrens = models.CharField(max_length=30, default=0)
 
+    def __str__(self):
+        return self.name
 
-
-
-
+    # def defining_childrens(self):
+    #     for article_ in Article.objects.all():
+    #         if article_.parent_name == self.name:
+    #             return article_.name
--- My_First_WEB/django_tutorial/views.py
+++ My_First_WEB/django_tutorial/views.py
@@ -1,7 +1,17 @@
+from django import db
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from django_tutorial.models import *
 
 
 def index_djtut(reqest):
-    return render(reqest, 'django_tutorial/index.html')
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+        'title': 'Учебные материалы'
+    }
+
+    return render(reqest, 'django_tutorial/index.html',  context=context)
+
+
