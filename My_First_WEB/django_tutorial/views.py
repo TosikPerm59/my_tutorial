d53a420e1a2d691b75a1a3c1ae63c9f678fbaa51
TosikPerm59 <tosikperm59@gmail.com>
2021-07-30 00:16:12 +0500

    Добавил поисковую строку в head настроил представление поиска и передачу запроса, дальше нужно написать сам поиск

--- My_First_WEB/django_tutorial/views.py
+++ My_First_WEB/django_tutorial/views.py
@@ -1,5 +1,6 @@
-from django import db
+
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
 
 from django_tutorial.Templates.temlatetags.django_tutorial_tags import get_articles, links_list
 from django_tutorial.models import *
@@ -22,3 +23,21 @@ def show_article(reqest, article_id):
     }
 
     return render(reqest, 'django_tutorial/index.html', context=context)
+
+
+def search_results(reqest):
+    query = reqest.GET.get('search_string')
+    context = {
+        'articles': get_articles,
+        'title': 'Результат поиска',
+        'search_words': query
+    }
+    print(query)
+    return render(reqest, 'django_tutorial/index.html', context=context)
+
+
+
+
+
+
+
