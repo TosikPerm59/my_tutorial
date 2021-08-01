fd111908d3df13817645714c278a3ea30783dbcf
TosikPerm59 <tosikperm59@gmail.com>
2021-08-02 00:04:14 +0500

    Настроил поиск в названиях статей и в тексте статей. Добавил шаблон search_result.html Подкорректировал отображение блока контента при разных ситуациях

--- My_First_WEB/django_tutorial/views.py
+++ My_First_WEB/django_tutorial/views.py
@@ -1,7 +1,6 @@
 
 from django.shortcuts import render
 
-
 from django_tutorial.Templates.temlatetags.django_tutorial_tags import *
 
 
@@ -31,24 +30,26 @@ def search_results(request):
     search_query = request.GET.get('search_string')
     if search_query:
         search_words = search_query.split()
-        search_res = Article.objects.filter(name__contains=search_words[0] or search_words[1] or search_words[2])
+        search_res_in_name = Article.objects.filter(name__contains=search_words[0] or search_words[1] or search_words[2])
+        search_res_in_content = Article.objects.filter(content__contains=search_words[0] or search_words[1] or search_words[2])
 
     else:
         context = {
             'articles': get_articles,
-            'title': 'Учебные материалы'
+            'title': 'Результат поиска'
         }
-        return render(request, 'django_tutorial/index.html', context=context)
+        return render(request, 'django_tutorial/search_result.html', context=context)
 
 
     context = {
         'articles': get_articles,
         'title': 'Результат поиска',
         'search_words': search_query,
-        'search_res': search_res,
+        'search_res_in_name': search_res_in_name,
+        'search_res_in_content': search_res_in_content,
     }
 
-    return render(request, 'django_tutorial/index.html', context=context)
+    return render(request, 'django_tutorial/search_result.html', context=context)
 
 
 
